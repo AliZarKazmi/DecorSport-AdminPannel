ddaddfeb11fbd46a496041fc1ca4293f8ed322cb
AliZarKazmi <89480198+AliZarKazmi@users.noreply.github.com>
2023-06-23 03:59:07 +0500

    data fetch

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,4 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   FormControl,
   InputLabel,
@@ -7,120 +6,197 @@ import {
   MenuItem,
   TextField,
   Button,
-  Grid,
 } from '@material-ui/core';
-
+import Swal from "sweetalert2";
+import { db } from "../firebaseConfig/Firebase.js";
+import {
+  collection,
+  getDocs,
+  query,
+  where
+} from "firebase/firestore";
 
 function Searchbar() {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [selectedSubCategory, setSelectedSubCategory] = useState('');
-
+  const [searchKeyword, setSearchKeyword] = useState('');
+  
   const handleCategoryChange = (event) => {
     setSelectedCategory(event.target.value);
-    setSelectedSubCategory('');
+    setSelectedSubCategory(''); 
   };
 
   const handleSubCategoryChange = (event) => {
     setSelectedSubCategory(event.target.value);
   };
 
-  const renderSubCategoryOptions = () => {
-    if (selectedCategory === 'Profiles') {
-      return (
-        <Select
-          value={selectedSubCategory}
-          onChange={handleSubCategoryChange}
-          variant="outlined"
-          fullWidth
-        >
-          <MenuItem value="All">All</MenuItem>
-          <MenuItem value="Seller Account">Seller Account</MenuItem>
-          <MenuItem value="Decorator Account">Decorator Account</MenuItem>
-        </Select>
-      );
-    } else if (selectedCategory === 'Ideas') {
-      return (
-        <Select
-          value={selectedSubCategory}
-          onChange={handleSubCategoryChange}
-          variant="outlined"
-          fullWidth
-        >
-          <MenuItem value="All">All</MenuItem>
-          <MenuItem value="Bedroom">Occasion Ideas</MenuItem>
-          <MenuItem value="Study-room">Home Ideas</MenuItem>
-          {/* Additional sub-category options */}
-        </Select>
-      );
-    } else if (selectedCategory === 'Products') {
-      return (
-        <Select
-          value={selectedSubCategory}
-          onChange={handleSubCategoryChange}
-          variant="outlined"
-          fullWidth
-        >
-          <MenuItem value="All">All</MenuItem>
-          <MenuItem value="Furniture">Furniture</MenuItem>
-          <MenuItem value="Fabric Material">Fabric Collection</MenuItem>
-          <MenuItem value="Decoration Accessories">Decorated Accessories</MenuItem>
-          <MenuItem value="Decoration Covering">Decorated Covering</MenuItem>
-        </Select>
-      );
-    } else {
-      return null; // No sub-category options for other categories
+  const handleSearchKeywordChange = (event) => {
+    setSearchKeyword(event.target.value);
+  };
+
+  const applyFilters = async () => {
+  let filteredQuery;
+  
+  if(searchKeyword!="" && selectedCategory!="" && selectedSubCategory!="")
+  {
+    if("Profiles"==selectedCategory)
+    {
+      const dataRef= collection(db, "RegisterationForms");
+      
+       filteredQuery = query(dataRef, where('CompanyName', '==', searchKeyword), where('AccountType', '==', selectedSubCategory));  
+       
+      const filteredDataSnapshot = await getDocs(filteredQuery);
+
+      if(filteredDataSnapshot.size==0)
+      {
+         Swal.fire("Not Found!")
+      }
+      else{
+        const filteredData = filteredDataSnapshot.docs.map((doc) => doc.data());
+        console.log(filteredData); // Display filtered data on the console
+      }
     }
+
+    if("Products"==selectedCategory)
+    {
+      const dataRef= collection(db, "ProductList");
+  
+       filteredQuery = query(dataRef,  where('ProductName', '==', searchKeyword),where('Category', '==', selectedSubCategory));
+      
+      const filteredDataSnapshot = await getDocs(filteredQuery);
+
+      if(filteredDataSnapshot.size==0)
+      {
+         Swal.fire("Not Found!")
+      }
+      else{
+      const filteredData = filteredDataSnapshot.docs.map((doc) => doc.data());
+  
+      console.log(filteredData); // Display filtered data on the console
+    }
+  }
+  
+    if("Ideas"==selectedCategory)
+    {
+      const dataRef= collection(db, "IdeasList");
+  
+     
+          filteredQuery = query(dataRef,  where('SubCategory', '==', searchKeyword),where('Category', '==', selectedSubCategory));
+      
+      const filteredDataSnapshot = await getDocs(filteredQuery);
+      if(filteredDataSnapshot.size==0)
+      {
+         Swal.fire("Not Found!")
+      }
+      else{
+      const filteredData = filteredDataSnapshot.docs.map((doc) => doc.data());
+  
+      console.log(filteredData); // Display filtered data on the console
+    }
+  }
+
+  }
+  else
+  {
+    Swal.fire("Search Fields are Empty");
+  }
+  
+  if(selectedCategory=="" && selectedSubCategory=="" && searchKeyword=="")
+  {
+    Swal.fire("Not Found! ");
+  }
+   
   };
 
   return (
-   
-            <div style={{ backgroundColor: 'white', borderRadius: '8px', marginRight:"5px"}}>
-          <h2 style={{margin:"20px"}}>Filters</h2>
-          <form >
-            <FormControl  fullWidth style={{ margin:'15px',backgroundColor: 'white', borderRadius: '8px', }}>
-              <InputLabel htmlFor="search" ></InputLabel>
-              <TextField id="search" placeholder="Enter search keyword" variant="outlined" />
-            </FormControl>
+    <div style={{ backgroundColor: 'white', borderRadius: '8px', marginRight: "5px" }}>
+      <h2 style={{ margin: "20px" }}>Filters</h2>
+      <form>
+        <FormControl fullWidth style={{ margin: '15px', backgroundColor: 'white', borderRadius: '8px' }}>
+          {/* <InputLabel htmlFor="search">Search:</InputLabel> */}
+          <TextField
+            id="search"
+            placeholder="Enter search keyword"
+            variant="outlined"
+            value={searchKeyword}
+            onChange={handleSearchKeywordChange}
+          />
+        </FormControl>
 
-            <FormControl  fullWidth style={{ margin:'15px',backgroundColor: 'white', borderRadius: '8px', }}>
+        <FormControl fullWidth style={{ margin: '15px', backgroundColor: 'white', borderRadius: '8px' }}>
+          <InputLabel id="category">Category:</InputLabel>
+          <Select
+            labelId="category"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+            variant="outlined"
+            fullWidth
+          >
+            <MenuItem value="">All</MenuItem>
+            <MenuItem value="Profiles">Profiles</MenuItem>
+            <MenuItem value="Products">Products</MenuItem>
+            <MenuItem value="Ideas">Ideas</MenuItem>
+          </Select>
+        </FormControl>
 
-              <InputLabel id="category" style={{marginLeft: "12px" }}> Category:</InputLabel>
-              
-              <Select
-                labelId="category"
-                value={selectedCategory}
-                onChange={handleCategoryChange}
-                variant="outlined"
-                fullWidth
-              >
-                <MenuItem value="">All</MenuItem>
-                <MenuItem value="Profiles">Profiles</MenuItem>
-                <MenuItem value="Products">Products</MenuItem>
-                <MenuItem value="Ideas">Ideas</MenuItem>
-              </Select>
-            </FormControl>
-
-            <FormControl fullWidth style={{ margin:'15px',backgroundColor: 'white', borderRadius: '8px', }}>
-              <InputLabel id="sub-category" style={{marginLeft: "12px" }}> Sub-Category:</InputLabel>
-              {renderSubCategoryOptions()}
-            </FormControl>
-
-            <Button
-              variant="contained"
-              color="primary"
-              style={{ marginTop: '10px',marginLeft:"12px" }}
+        <FormControl fullWidth style={{ margin: '15px', backgroundColor: 'white', borderRadius: '8px' }}>
+          <InputLabel id="sub-category">Sub-Category:</InputLabel>
+          {selectedCategory === 'Profiles' && (
+            <Select
+              value={selectedSubCategory}
+              onChange={handleSubCategoryChange}
+              variant="outlined"
               fullWidth
             >
-              Apply Filters
-            </Button>
-          </form>
-        </div>
-      
+              <MenuItem value="All">All</MenuItem>
+              <MenuItem value="Product Seller">Product Seller</MenuItem>
+              <MenuItem value="Decorator">Decorator</MenuItem>
+            </Select>
+          )}
+          {selectedCategory === 'Ideas' && (
+            <Select
+              value={selectedSubCategory}
+              onChange={handleSubCategoryChange}
+              variant="outlined"
+              fullWidth
+            >
+              <MenuItem value="All">All</MenuItem>
+              <MenuItem value="Occasion Decoration Ideas">Occasion Decoration Ideas</MenuItem>
+              <MenuItem value="Home Decoration Ideas">Home Decoration Ideas</MenuItem>
+              {/* Additional sub-category options */}
+            </Select>
+          )}
+          {selectedCategory === 'Products' && (
+            <Select
+              value={selectedSubCategory}
+              onChange={handleSubCategoryChange}
+              variant="outlined"
+              fullWidth
+            >
+              <MenuItem value="All">All</MenuItem>
+              <MenuItem value="Furniture">Furniture</MenuItem>
+              <MenuItem value="Fabric Material">Fabric Collection</MenuItem>
+              <MenuItem value="Decoration Accessories">Decoration Accessories</MenuItem>
+              <MenuItem value="Decorated Coverings">Decorated Coverings</MenuItem>
+            </Select>
+          )}
+        </FormControl>
+
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginTop: '10px', marginLeft: "12px" }}
+          fullWidth
+          onClick={applyFilters}
+        >
+          Apply Filters
+        </Button>
+      </form>
+    </div>
   );
 }
 
 export default Searchbar;
-
 
 
 
